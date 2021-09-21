efb826c65b71e4393dd55a94e85039f8c7d8236f
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-09-21 19:08:41 -0400

    installed needed apollo/client and apollo/react-hooks and added useMutation on SearchBooks.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    _id
+    username
+    email
+    password
+    savedBooks {
+      _id
+      authors
+      description
+      bookId
+      image
+      link
+      title
+    }
+  }
+`;
